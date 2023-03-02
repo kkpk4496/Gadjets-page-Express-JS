58a2285f7cf8f4d8736655fc3e35531c626056a6
PavanKalyan <pavankalyank4496@gmail.com>
2023-03-02 04:03:36 +0000

    Solution

---
+++ app.js
@@ -0,0 +1,8 @@
+let express = require("express");
+let app = express();
+
+app.get("/gadgets", (request, response) => {
+  response.sendFile("./gadgets.html", { root: __dirname });
+});
+
+module.exports = app;
